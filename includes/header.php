8e7e658d29cc2b9d2d83e8bcbdcb13383783a417
Doug Morgan <dougbmorgan@me.com>
2015-08-21 17:53:13 -0400

    add new repository

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header>
+	<div id="header-img">
+  	<nav class="darkgreenBg fullContainer"></nav>
+		<div class="content_wrapper">
+			<h3>CRI New Hire</h3>
+			<a class="arrow" href="#trees"><i class="fa fa-angle-down fa-4x"></i><br />Scroll</a>
+		</div>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if IE 9 ]>        <html class="no-js ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="mobile-web-app-capable" content="yes">
+
+		<title>Hire Me</title>
+
+		<!-- core CSS -->
+		<link rel="stylesheet" href="css/main.css">
+
+		<!-- build:js -->
+		<script src="js/modernizr/modernizr.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/master.js"></script>
+		<!-- endbuild -->
+
+		<!--[if (gte IE 6)&(lte IE 8)]>
+		  <script type="text/javascript" src="js/selectivizr/selectivizr.js"></script>
+		  <noscript><link rel="stylesheet" href="ie.css" /></noscript>
+		<![endif]-->
+	</head>
+
+	<body>
+	  <?php include("includes/header.php"); ?>
+
+		<section id="trees" class="section trees turqoisBg">
+			<div class="content_wrapper">
+				<div class="container">
+				  <ul class="row">
+					  <li class="grayscale col--3 col">&nbsp;</li>
+					  <li class="dark col--3 col">&nbsp;</li>
+						<li class="sepia col--3 col">&nbsp;</li>
+					</ul>
+				</div>
+			</div><!-- /end content_wrapper -->
+		</section>
+
+		<section id="graph" class="section graph">
+			<div class="content_wrapper">
+			  <div class="container">
+				  <ul class="row">
+					  <li class="graph1 col--3 col">&nbsp;</li>
+					  <li class="graph2 col--3 col">&nbsp;</li>
+						<li class="graph3 col--3 col">&nbsp;</li>
+						<li class="graph4 col--3 col">&nbsp;</li>
+					</ul>
+				</div>
+			</div><!-- /end content_wrapper -->
+		</section>
+
+		<footer class="fullContainer blackBg">&nbsp;</footer>
+	</body>
+</html>
